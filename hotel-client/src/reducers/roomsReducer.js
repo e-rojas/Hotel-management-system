7dae5f569cd4fa400eacbf74c30c90e66c9255f7
javascript01fan <ejrojas@icloud.com>
2020-04-16 09:11:58 -0300

    page added

--- hotel-client/src/reducers/roomsReducer.js
+++ hotel-client/src/reducers/roomsReducer.js
@@ -1,8 +1,12 @@
-const initialState = [];
+const initialState = {
+  data: [],
+};
 const roomReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SEARCH':
-      return [...state, ...action.updates];
+      return {
+        data: [...state, ...action.updates],
+      };
     default:
       return state;
   }
